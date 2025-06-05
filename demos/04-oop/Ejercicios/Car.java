39062e97f99c6327695db766cf212791c5184ea0
Stefania Buri <stefania_0506@hotmail.com>
2025-06-05 13:43:22 +0200

    Update way to print on screen with method

--- demos/04-oop/Ejercicios/Car.java
+++ demos/04-oop/Ejercicios/Car.java
@@ -7,6 +7,7 @@ public class Car {
     boolean isAuto;
     char type;
 
+    // Constructor
     Car(String model, String color, int seats, int numDoor, boolean isAuto, char type) {
         this.model = model;
         this.color = color;
@@ -14,5 +15,17 @@ public class Car {
         this.numDoor = numDoor;
         this.isAuto = isAuto;
         this.type = type;
+    }
+
+    void showInfo() {
+        String message = """
+                Model is: %s
+                Color is: %s
+                Number of seats: %d
+                Number of door: %d
+                Is Auto?: %b
+                Type: %c
+                        """.formatted(model, color, seats, numDoor, isAuto, type);
+        System.out.println(message);
     }
 }
--- demos/04-oop/Ejercicios/Main.java
+++ demos/04-oop/Ejercicios/Main.java
@@ -5,12 +5,16 @@ public class Main {
         Car car1;
 
         car1 = new Car("Kia", "white", 5, 4, false, 'F');
-        System.out.println("Model is: " + car1.model);
-        System.out.println("Color:" + car1.color);
-        System.out.println("Seats:" + car1.seats);
-        System.out.println("Number of door:" + car1.numDoor);
-        System.out.println("Is auto? " + car1.isAuto);
-        System.out.println("Type " + car1.type);
+        /*
+         * System.out.println("Model is: " + car1.model);
+         * System.out.println("Color:" + car1.color);
+         * System.out.println("Seats:" + car1.seats);
+         * System.out.println("Number of door:" + car1.numDoor);
+         * System.out.println("Is auto? " + car1.isAuto);
+         * System.out.println("Type " + car1.type);
+         */
 
+        // Mostrando con un método que esta dentro de la clase Car
+        car1.showInfo();
     }
 }
