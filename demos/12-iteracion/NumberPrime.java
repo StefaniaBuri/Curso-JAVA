6b94e44aac62d3e643ee16e813ff712b94de17fe
Stefania Buri <stefania_0506@hotmail.com>
2025-06-16 11:35:35 +0200

    update number prime list

--- demos/12-iteracion/NumberPrime.java
+++ demos/12-iteracion/NumberPrime.java
@@ -2,32 +2,43 @@ import java.util.Scanner;
 
 public class NumberPrime {
 
-    static int getNumByScanner() {
-
+    static int getNumByScanner(String message) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Dime un número para ver si es primo: ");
+        System.out.println(message);
         int num1 = scanner.nextInt();
         scanner.close();
         return num1;
     }
 
     public static boolean isPrime(int number) {
-        for (int index = 1; index < number; index++) {
-            if(number % index == 0) {
-                return false; //No es primo porque el resto es 0
+        for (int index = 2; index < number; index++) {
+            if (number % index == 0) {
+                return false; // No es primo porque el resto es 0
             }
         }
         return true; // es primo
     }
 
-    static void showInfo() {
-        int number = getNumByScanner();
-        String result = isPrime(number) ? "%s es primo" : "%s no es primo";
+    static boolean isPrime2(int num) {
+        if (num < 2)
+            return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+
+    static void showInfo(int number) {
+        // int number = getNumByScanner();
+        String result = isPrime2(number) ? "%s es primo" : "%s no es primo";
         System.out.printf(result, number);
 
     }
 
     public static void main(String[] args) {
-        showInfo();
+        String message = "Dime un número para ver si es primo: ";
+        showInfo(getNumByScanner(message));
     }
 }
--- demos/12-iteracion/NumberPrimeList.java
+++ demos/12-iteracion/NumberPrimeList.java
@@ -1,44 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class NumberPrimeList {
-    static int getNumByScanner() {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Dime un número para darte un la lista de sus primos");
-        int num1 = scanner.nextInt();
-        scanner.close();
-        return num1;
+    // OPTION 2
+    static void showPrimes(List<Integer> numbers) {
+        /* for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        } */
+       for(int number : numbers) {
+        System.out.println(number);
+       }
     }
 
-    static void showPrime(int number) {
-        for (int index = 1; index < number; index++) {
-            if(NumberPrime.isPrime(index)) {
-                System.out.println(index);
-            }
-        }
-    }
 
-    static List<Integer> calculatePrime(int number) {
-        List<Integer> result = new ArrayList<Integer>();
+    //OPTION 2
+    static List<Integer> calculatePrimes2(int number) {
+        List<Integer> result = new ArrayList<>();
         for (int index = 1; index < number; index++) {
-            if(NumberPrime.isPrime(index)) {
+            if (NumberPrime.isPrime(index)) {
                 result.add(index);
             }
         }
         return result;
     }
 
-    /* static void showInfo(int number) {
-        if(showPrime(number) != 0) {
-            System.out.println(number);
-        }
-        
 
+     /* static List<Integer> calculatePrime(int number) {
+        List<Integer> result = new ArrayList<>();
+        for (int index = 2; index <= number; index++) {
+            if (NumberPrime. isPrime2(index)) {
+                result.add(index);
+            }
+        }
+        return result;
     } */
+   
 
     public static void main(String[] args) {
-        
+        String message = "Dime un número para darte un la lista de sus primos";
+        int number = NumberPrime.getNumByScanner(message);
+
+        //List<Integer> numbers = calculatePrime(number);
+        List<Integer> numbers = calculatePrimes2(number); //OPTION 2
+        //System.out.println("Lista de números primos hasta " + number + ":" + numbers);
+        showPrimes(numbers); //OPTION 2
+    
     }
 }
